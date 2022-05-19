0ec9d641b66c81d81b91b9b116b0a6ef0f8accb9
Udaysass <udayv.sass@gmail.com>
2022-05-19 15:40:31 +0530

    api-emp

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -6,16 +6,15 @@ module Api
       # before_action :authenticate_employee!
       # load_and_authorize_resource
       before_action :set_employee, only: [:update]
-      protect_from_forgery with: :null_session
+      skip_before_action :verify_authenticity_token
 
       # GET /employees/1 or /employees/1.json
       def show
         @employee = get_employee
-        render json: EmployeeSerializer.new(employee, {} ) 
-        # render json: {
-        #   data: serializer_data(@employee),
-        #   message: ['employee profile successfully update'], status: 200, type: 'Success'
-        # }
+        render json: {
+          data: serializer_data(@employee),
+          message: ['employee profile '], status: 200, type: 'Success'
+        }
       end
 
       # PATCH /employees/1 or /employees/1.json
@@ -35,13 +34,8 @@ module Api
 
       # Use callbacks to share common setup or constraints between actions.
       def set_employee
-        @employee = Employee.find(params[:id])
+        @employee = Employee.find_by_id(params[:id])
         render json: { message: ['Employee not found'] }, status: 404 unless @employee.present?
-      end
-
-      def serializer_data(employee)
-        byebug
-        ActiveModelSerializers::SerializableResource.new(employee, serializer: EmployeeSerializer)
       end
 
       def employee_params
