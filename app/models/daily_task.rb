2a49fd712fa1e9c8f45dfff25b6d3cc4f6da4626
Namrata12gupta <namratag.sass@gmail.com>
2022-07-01 17:32:22 +0530

    done dailytask functionality

--- app/models/daily_task.rb
+++ app/models/daily_task.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class DailyTask < ApplicationRecord
-  belongs_to :employee
+  belongs_to :employee, dependent: :destroy
   validates :description, presence: true
   paginates_per 10
+
+  def self.search(search)
+    if search
+      daily_task = []
+      search.each do |s|
+        daily_task << DailyTask.joins(:employee).where('employees.name LIKE ?', "%#{s}%")
+      end
+      daily_task.flatten
+    else
+      all
+    end
+  end
 end
